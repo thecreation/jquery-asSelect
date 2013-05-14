4c3760f39d2aedee259f52ddffa1e52064b7d6a9
joeylin <joe.y.lin.2012@gmail.com>
2013-05-14 03:56:26 -0700

    add opts group

--- src/jquery-select.js
+++ src/jquery-select.js
@@ -9,26 +9,35 @@
 (function($) {
 
     var Select = $.select = function(element, options) {
-        var meta = {};
+        var $opts,
+            meta = {};
 
         this.element = element;
         this.$element = $(element);
         this.$options = this.$element.find('option');
         this.$optgroups = this.$element.find('optgroup');
-        this.group = [];
+        this.state = {};
+
+        meta.state = {};
+        meta.value = '';
 
         if (this.$optgroups.length !== 0) {
             $.each(this.$optgroup, function(i,v) {
-                this.group.push($(v).find('option'));
+                var label = $(v).attr('label');
+                meta.state[label] = {};
+                $.each($(v).find('options'),function(i,v) {
+                    meta.state[label][$(v).attr('value')] = $(v).text(); 
+                    if ($(v).prop('selected')) {
+                        meta.value = $(v).attr('value');
+                    }
+                });
             });
         }
 
-        if (this.$options.length !== 0) {
-            meta.opts = {};
-            meta.value = '';
-
-            $.each(this.$options, function(i, v) {
-                meta.options[$(v).attr('value')] = $(v).text();
+        $opts = this.$element.find('> options');
+        if ($opts.length !== 0) {
+            $.each($opts, function(i, v) {
+                meta.opts[$(v).attr('value')] = $(v).text();
                 if ($(v).prop('selected')) {
                     meta.value = $(v).attr('value');
                 }
@@ -37,6 +46,7 @@
 
         this.options = $.extend({}, Select.defaults, options, meta);
         this.value = this.options.value;
+        this.state = this.options.state;
 
         // flag
         this.opened = false;
@@ -54,15 +64,25 @@
             this.$bar = this.$select.find('.select-bar');
             this.$content = this.$select.find('.select-content');
 
-            $.each(this.options.opts,function(key,value) {
-                var $li = $('<li></li>').data('value',key).text(value);
-
-                if (self.value === key) {
-                    $tpl.addClass('select-active');
+            $.each(this.state,function(key,value) {
+                
+                if (typeof value === 'object') {
+                    var $group = $('<li class="select-group"></li>').text(key);
+                    self.$content.append($group);
+                    $.each(value, function(k, v) {
+                        var $li = $('<li class="group-item"></li>').data('value',k).text(v);
+                        if (self.value === key) {
+                            $li.addClass('select-active');
+                        }
+                        self.$content.append($li);
+                    });
+                } else {
+                    var $li = $('<li></li>').data('value',key).text(value);
+                    if (self.value === key) {
+                        $li.addClass('select-active');
+                    }
+                    self.$content.append($li);
                 }
-
-                self.$content.append($li);
-
             });
 
             this.$li = this.$content.find('li');
@@ -77,9 +97,11 @@
 
             this.$content.delegate('click', 'li', function() {
                 var value = $(this).data(value);
+
                 if (value === undefined) {
                     return
                 }
+
                 self.set.call(self, value);
             });
         },
@@ -88,10 +110,12 @@
             this.$content.css({display: 'block'});
             this.opened = true;
         },
+
         hide: function() {
             this.$content.css({display: 'none'});
             this.opened = false;
         },
+
         set: function(value) {
             var self = this,
                 content;
@@ -112,6 +136,17 @@
 
             this.value = value;
             this.hide();
+        },
+        get: function() {
+            var self = this,
+                value;
+                
+            $.each(this.$options, function(i, v) {
+                if ($(v).data('value') === self.value) {
+                    value = $(v).text();
+                }
+            });
+            return value;
         }
 
     };
