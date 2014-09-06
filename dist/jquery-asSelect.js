a734b7deabfb1f75ab17ca64048f3ab6c7143919
amazingSurge <amazingSurge@yahoo.com>
2014-09-06 04:17:31 -0700

    update trigger func

--- dist/jquery-asSelect.js
+++ dist/jquery-asSelect.js
@@ -100,7 +100,7 @@
         },
         _trigger: function(eventType) {
             var method_arguments = Array.prototype.slice.call(arguments, 1),
-                data = method_arguments.concat([this]);
+                data = [this].concat(method_arguments);
 
             // event
             this.$select.trigger('asSelect::' + eventType, data);
